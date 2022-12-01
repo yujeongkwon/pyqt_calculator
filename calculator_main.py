ea2daf17e0fe4723386e0a9faeda06138386b03b
yujeongkwon <yoojeongkwon@naver.com>
2022-12-02 02:33:39 +0900

    design: 기존 버튼 배치 변경
    사칙연산 버튼 등 기존 버튼을 윈도우 계산기 위치로 배치 변경하기
    
    Issues: #8

--- calculator_main.py
+++ calculator_main.py
@@ -10,9 +10,7 @@ class Main(QDialog):
         main_layout = QVBoxLayout()
 
         ### 각 위젯을 배치할 레이아웃을 미리 만들어 둠
-        layout_operation = QGridLayout()
-        layout_clear_equal = QGridLayout()
-        layout_number = QGridLayout()
+        layout_button = QGridLayout()
         layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
@@ -29,7 +27,7 @@ class Main(QDialog):
         button_plus = QPushButton("+")
         button_minus = QPushButton("-")
         button_product = QPushButton("x")
-        button_division = QPushButton("/")
+        button_division = QPushButton("÷")
 
         ### 사칙연산 버튼을 클릭했을 때, 각 사칙연산 부호가 수식창에 추가될 수 있도록 시그널 설정
         button_plus.clicked.connect(lambda state, operation = "+": self.button_operation_clicked(operation))
@@ -37,11 +35,11 @@ class Main(QDialog):
         button_product.clicked.connect(lambda state, operation = "*": self.button_operation_clicked(operation))
         button_division.clicked.connect(lambda state, operation = "/": self.button_operation_clicked(operation))
 
-        ### 사칙연산 버튼을 layout_operation 레이아웃에 추가
-        layout_operation.addWidget(button_plus)
-        layout_operation.addWidget(button_minus)
-        layout_operation.addWidget(button_product)
-        layout_operation.addWidget(button_division)
+        ### 사칙연산 버튼을 layout_button 레이아웃에 추가
+        layout_button.addWidget(button_plus,4,3)
+        layout_button.addWidget(button_minus,3,3)
+        layout_button.addWidget(button_product,2,3)
+        layout_button.addWidget(button_division,1,3)
 
         ### =, clear, backspace 버튼 생성
         button_equal = QPushButton("=")
@@ -53,12 +51,12 @@ class Main(QDialog):
         button_clear.clicked.connect(self.button_clear_clicked)
         button_backspace.clicked.connect(self.button_backspace_clicked)
 
-        ### =, clear, backspace 버튼을 layout_clear_equal 레이아웃에 추가
-        layout_clear_equal.addWidget(button_clear)
-        layout_clear_equal.addWidget(button_backspace)
-        layout_clear_equal.addWidget(button_equal)
+        ### =, clear, backspace 버튼을 layout_button 레이아웃에 추가
+        layout_button.addWidget(button_clear,0,2)
+        layout_button.addWidget(button_backspace,0,3)
+        layout_button.addWidget(button_equal,5,3)
 
-        ### 숫자 버튼 생성하고, layout_number 레이아웃에 추가
+        ### 숫자 버튼 생성하고, layout_button 레이아웃에 추가
         ### 각 숫자 버튼을 클릭했을 때, 숫자가 수식창에 입력 될 수 있도록 시그널 설정
         number_button_dict = {}
         for number in range(0, 10):
@@ -67,24 +65,22 @@ class Main(QDialog):
                                                        self.number_button_clicked(num))
             if number >0:
                 x,y = divmod(number-1, 3)
-                layout_number.addWidget(number_button_dict[number], x, y)
+                layout_button.addWidget(number_button_dict[number], x+2, y)
             elif number==0:
-                layout_number.addWidget(number_button_dict[number], 3, 1)
+                layout_button.addWidget(number_button_dict[number], 5, 1)
 
         ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
         button_dot = QPushButton(".")
         button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
-        layout_number.addWidget(button_dot, 3, 2)
+        layout_button.addWidget(button_dot, 5, 2)
 
         button_double_zero = QPushButton("00")
         button_double_zero.clicked.connect(lambda state, num = "00": self.number_button_clicked(num))
-        layout_number.addWidget(button_double_zero, 3, 0)
+        layout_button.addWidget(button_double_zero, 5, 0)
 
         ### 각 레이아웃을 main_layout 레이아웃에 추가
         main_layout.addLayout(layout_equation_solution)
-        main_layout.addLayout(layout_operation)
-        main_layout.addLayout(layout_clear_equal)
-        main_layout.addLayout(layout_number)
+        main_layout.addLayout(layout_button)
 
         self.setLayout(main_layout)
         self.show()
