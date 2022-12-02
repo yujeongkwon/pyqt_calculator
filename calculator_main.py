dc1507f47fd1f233e2c11cb44373d1ed9912c7df
yujeongkwon <yoojeongkwon@naver.com>
2022-12-02 23:48:07 +0900

    design : 숫자 버튼 배치 변경
    윈도우 계산기 처럼 변경
    
    Issues: #8

--- calculator_main.py
+++ calculator_main.py
@@ -79,7 +79,7 @@ class Main(QDialog):
                                                        self.number_button_clicked(num))
             if number >0:
                 x,y = divmod(number-1, 3)
-                layout_button.addWidget(number_button_dict[number], x+2, y)
+                layout_button.addWidget(number_button_dict[number], 4-x, y)
             elif number==0:
                 layout_button.addWidget(number_button_dict[number], 5, 1)
 
